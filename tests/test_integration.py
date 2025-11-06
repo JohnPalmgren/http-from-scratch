1a7d803850ab67dc2d2d8e7c44ad4d0065bfc283
John Palmgren <johnmpalmgren@gmail.com>
2025-11-06 17:10:30 +0000

    add playwright test for integration

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,34 +1,8 @@
-import socket
-import pytest
+from playwright.sync_api import Page, expect
 
-# Mimic a client sending a request to test server returns expected result
-def test_server_response(running_server):
-    """Test if the server sends the 'Hello world' response."""
+def test_homepage_title(running_server, page):
+    base_url = running_server
+
+    page.goto(base_url + "/") 
     
-    # The 'running_server' argument is automatically filled by pytest
-    # with the 'port' value we yielded from the fixture.
-    port = running_server
-    response = b""
-    try:
-        # Create a client socket
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect(('localhost', port))
-        
-        # Send a simple HTTP request
-        request = b"GET / HTTP/1.1\r\nHost: localhost\r\n\r\n"
-        client_socket.sendall(request)
-        
-        # Receive the response
-        while True:
-            data = client_socket.recv(1024)
-            if not data:
-                break
-            response += data
-            
-        client_socket.close()
-
-    except Exception as e:
-        # 'pytest.fail' is the equivalent of 'self.fail'
-        pytest.fail(f"Client connection failed: {e}")
-
-    assert b"HTTP/1.1 200 OK" in response
+    expect(page).to_have_title("My website")
