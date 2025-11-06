690ca6925f8b0efb56896c47ed5d43a3fdb301ef
John Palmgren <johnmpalmgren@gmail.com>
2025-11-06 16:12:01 +0000

    Add 404 page

--- lib/routes.py
+++ lib/routes.py
@@ -11,7 +11,8 @@ class Routes:
             case "/pokemon":
                 file_name = "templates/pokemon/index.html"
             case _:
-                return "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+                return self._page_not_found()
+
 
         try:
             with open(file_name, "r") as file:
@@ -23,7 +24,18 @@ class Routes:
             return response
 
         except FileNotFoundError:
-            print("Error file not found")
-            # TODO implement generic 404
+            return self._page_not_found()
+
+    def _page_not_found(self):
+        header = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+        file_name = "templates/errors/not_found.html"
+        try:
+            with open(file_name, "r") as file:
+                html_body = file.read()
+
+            response = header + html_body
+            return response
+
+        except FileNotFoundError:
             header = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
             return header
