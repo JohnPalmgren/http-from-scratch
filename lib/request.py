07fcfb5b17ad989a673b96f726f899e925dd75ee
John Palmgren <johnmpalmgren@gmail.com>
2025-11-06 15:27:23 +0000

    change handle_request to static method

--- lib/request.py
+++ lib/request.py
@@ -1,7 +1,9 @@
 from lib.routes import Routes
 
 class Request():
-    def handle_request(self, client_socket):
+
+    @staticmethod
+    def handle_request(client_socket):
         request = client_socket.recv(1024).decode('utf-8')
         if request:
             routes = Routes(request)
@@ -14,5 +16,4 @@ class Request():
             client_socket.close()
             print(f'Received request {request.split()[1]}')
         else:
-            print('Request empty')
             client_socket.close()
