a0e07ac2e3aa8d7c834f87e92b69d0f480008250
John Palmgren <johnmpalmgren@gmail.com>
2025-11-07 07:53:27 +0000

    add playwright integration tests for routes

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,7 +2,31 @@ from playwright.sync_api import Page, expect
 
 def test_homepage_title(running_server, page):
     base_url = running_server
-
     page.goto(base_url + "/") 
-    
     expect(page).to_have_title("My website")
+
+def test_homepage_content(running_server, page):
+    base_url = running_server
+    page.goto(base_url + "/") 
+    heading = page.locator("h1")
+    expect(heading).to_have_text("Home page!")
+
+def test_pokemon_page(running_server, page):
+    base_url = running_server
+    page.goto(base_url + "/pokemon") 
+    list_item = page.locator("li")
+    expect(list_item).to_have_text([
+        "Charizard",
+        "Lucario",
+        "Umbreon",
+        "Pikachu"
+    ])
+
+def test_custom_page_not_found(running_server, page):
+    base_url = running_server
+    page.goto(base_url + "/non-existent-page")
+    heading = page.locator("h1")
+    sub_heading = page.locator("h2")
+    expect(heading).to_have_text("404")
+    expect(sub_heading).to_have_text("Page not found")
+
